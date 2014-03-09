0748a026c41885e09a806e8a315e92b339b63598
Guilherme Souza <souza.guilherme18@gmail.com>
2014-03-09 16:34:24 -0300

    Commit inicial

---
+++ TCC/Controllers/ProfessionalController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Front_End.Controllers
+{
+    public class ProfessionalController : Controller
+    {
+        //
+        // GET: /Professional/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //Evento de teste
+        public ActionResult Sum(int nro1, int nro2)
+        {
+            var value = nro1 + nro2;
+
+            ViewBag.SumResult = value;
+
+            return View();
+        }
+	}
+}
---
+++ TCC/Scripts/Customized.js
@@ -0,0 +1,34 @@
+﻿
+//date masked fields
+$(document).ready(function () {
+    $('.date').mask('00/00/0000');
+});
+
+//date picker fields
+$('.datepicker').datepicker();
+
+
+//on click text field do not hide navbar
+$('.dropdown-menu  input').click(function (e) {
+    e.stopPropagation();
+});
+
+
+jQuery(function ($) {
+    $('div.btn-group[data-toggle-name]').each(function () {
+        var group = $(this);
+        var form = group.parents('form').eq(0);
+        var name = group.attr('data-toggle-name');
+        var hidden = $('input[name="' + name + '"]', form);
+        $('button', group).each(function () {
+            var button = $(this);
+            button.live('click', function () {
+                hidden.val($(this).val());
+                alert(hidden.val());
+            });
+            if (button.val() == hidden.val()) {
+                button.addClass('active');
+            }
+        });
+    });
+});
